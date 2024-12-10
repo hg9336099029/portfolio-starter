1b964001c10d79f7791cd2d9f94ad524fcadadc7
harsh <hg3172003@gmail.com>
2024-12-11 01:33:08 +0530

    1-up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './App.css'
+import Intro from './components/Intro/Intro';
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      Subscribe ZainKeepsCode
+      <Navbar/>
+      <Intro/>
     </div>
   );
 }
